0886c9ecf7aabd6e16eae150d24b7c3e1bcfff98
dagmawit abeselom <115341732+dagimawit@users.noreply.github.com>
2023-12-05 09:53:12 +0300

    Added solution for Multiply Strings

--- C:\Users\Dagim\Desktop\a2s/multiply-strings.py
+++ C:\Users\Dagim\Desktop\a2s/multiply-strings.py
@@ -1,4 +1,19 @@
-class Solution(object):
-    def multiply(self, num1, num2):
-        result = int(num1)*int(num2)
-        return str(result)
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1,num2]:
+            return "0"
+        res = [0]*(len(num1) + len(num2))
+        num1,num2 = num1[::-1], num2[::-1]
+        
+        for d1 in range(len(num1)):
+            for d2 in range(len(num2)):
+                digit = int(num1[d1]) * (int(num2[d2]))
+                res[d1+d2] +=digit
+                res[d1+d2+1] +=(res[d1+d2] // 10)
+                res[d1+d2] = res[d1+d2]%10
+        res,beg = res[::-1],0
+        while beg < len(res) and res[beg] == 0:
+            beg+=1
+        res = map(str,res[beg:])
+        return "".join(res)
+        
