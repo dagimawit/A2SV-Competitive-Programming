2d287659e88fcc19bc2521e11a55b8e7fa09be61
dagmawit abeselom <115341732+dagimawit@users.noreply.github.com>
2024-03-13 17:53:40 +0300

    Add solution for Search Insert Position

--- leetcode/leetcode/search-insert-position.py
+++ leetcode/leetcode/search-insert-position.py
@@ -1,15 +1,9 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        #binary searchh means a search by dividing by half the array/list and searching ateratively
-        left = 0
-        right = len(nums)
-        
-        while left <= right  and left < len(nums) and right >= 0:
-            mid = (left + right) // 2
-            if target == nums[mid]:
-                return mid
-            if target < nums[mid]:
-                right = mid -1
-            else:
-                left = mid + 1
-        return left
+        n = len(nums)-1
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+            elif nums[n] < target:
+                return  n +1
+                
